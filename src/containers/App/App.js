a6d30c661ada5a7a221a0cdda8eda7da3aef65ec
Herman Chan <hermchan89@gmail.com>
2018-02-21 18:21:30 -0800

    request reddit api with axios

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import LeftPanel from '../../components/LeftPanel/LeftPanel';
+import MainPanel from '../../components/MainPanel/MainPanel';
+import axios from 'axios';
+import styles from './App.scss';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      posts: []
+    };
   }
+
+  componentDidMount() {
+    axios.get(`http://www.reddit.com/r/popular.json`)
+      .then(res => {
+        const data = res.data.data.children.map(obj => obj.data);
+        this.setState({ posts : data });
+      });
+  }
+
 
   render() {
     const sideBarData = [
@@ -16,8 +31,9 @@ class App extends React.Component {
     ];
 
     return (
-      <div>
+      <div className={styles.container}>
         <LeftPanel title={sideBarData} />
+        <MainPanel subreddit={this.state.posts} />
       </div>
     );
   }
