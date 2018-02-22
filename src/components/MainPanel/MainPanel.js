008922baf2cfed4b3bc081646bb5b8977ff10751
Herman Chan <hermchan89@gmail.com>
2018-02-21 19:40:34 -0800

    flex styled content

--- src/components/MainPanel/MainPanel.js
+++ src/components/MainPanel/MainPanel.js
@@ -11,15 +11,20 @@ const MainPanel = (props) => {
         <ul>
           {props.subreddit.map((post) =>
             <li className={styles.items} key={post.id}>
-                <span className={styles.title}>
-                  <a href={ `${post.url}` }>{post.title}</a>
-                </span>
-                <span className={styles.info}>
-                  <a href={ `${post.url}` }>{post.score} points by {post.author} </a>
-                </span>
-                <span className={styles.info}>
-                  <a href={'https://www.reddit.com' + `${post.permalink}`}>{post.num_comments} comments</a>
-                </span>
+              <img className={styles.image} src={ `${post.thumbnail}` } height="70" width="70" />
+                <div className={styles.content}>
+                  <span className={styles.title}>
+                    <a href={ `${post.url}` }>{post.title}</a>
+                  </span>
+                  <div className={styles.info}>
+                    <span>
+                      <a href={ `${post.url}` }>{post.score} points by {post.author} </a>
+                    </span>
+                    <span className={styles.comment}>
+                      <a href={'https://www.reddit.com' + `${post.permalink}`}>{post.num_comments} comments</a>
+                    </span>
+                  </div>
+                </div>
             </li>
           )}
         </ul>
