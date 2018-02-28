249dff450a207f23b30c4e5f34b2ed67f21ad84b
Herman Chan <hermchan89@gmail.com>
2018-02-27 17:43:23 -0800

    axios request to product hunts api

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -8,18 +8,33 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: []
+      reddit: [],
+      productHunt: []
     };
   }
 
+
   componentDidMount() {
+    const devToken = '4749186aeaf490a8ef0d03677a081245d300de9797c5affeadaed87cc6afc611';
+
     axios.get(`http://www.reddit.com/r/popular.json`)
       .then(res => {
         const data = res.data.data.children.map(obj => obj.data);
-        this.setState({ posts : data });
+        this.setState({ reddit : data });
+      })
+      .catch(error => console.log(error, "ERROR"))
+
+    axios.get('https://api.producthunt.com/v1/posts', {
+      headers: { 'Authorization': 'Bearer'+` ${devToken}` }
+      })
+      .then(res => {
+        const post = res.data.posts;
+        this.setState({ productHunt : post });
+      })
+      .catch(err => {
+        console.log(err);
       });
   }
-
 
   render() {
     const sideBarData = [
@@ -33,7 +48,10 @@ class App extends React.Component {
     return (
       <div className={styles.container}>
         <LeftPanel title={sideBarData} />
-        <MainPanel subreddit={this.state.posts} />
+        <MainPanel
+          reddit={this.state.reddit}
+          productHunt={this.state.productHunt}
+        />
       </div>
     );
   }
