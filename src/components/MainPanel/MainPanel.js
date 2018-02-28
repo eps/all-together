86283a636cc1070a39841d4a65c5c0a8891768a2
Herman Chan <hermchan89@gmail.com>
2018-02-27 17:46:13 -0800

    restructure main panel components into smaller components

--- src/components/MainPanel/MainPanel.js
+++ src/components/MainPanel/MainPanel.js
@@ -2,39 +2,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './MainPanel.scss';
+import RedditPanel from '../RedditPanel/RedditPanel';
 
 const MainPanel = (props) => {
-  console.log(props);
+  const { reddit, productHunt } = props;
+  console.log('product-hunt', productHunt);
     return (
-    <div className={styles.mainPanel}>
-      <div className={styles.itemList}>
-        <ul>
-          {props.subreddit.map((post) =>
-            <li className={styles.items} key={post.id}>
-              <img className={styles.image} src={ `${post.thumbnail}` } height="70" width="70" />
-                <div className={styles.content}>
-                  <span className={styles.title}>
-                    <a href={ `${post.url}` }>{post.title}</a>
-                  </span>
-                  <div className={styles.info}>
-                    <span>
-                      <a href={ `${post.url}` }>{post.score} points by {post.author} </a>
-                    </span>
-                    <span className={styles.comment}>
-                      <a href={'https://www.reddit.com' + `${post.permalink}`}>{post.num_comments} comments</a>
-                    </span>
-                  </div>
-                </div>
-            </li>
-          )}
-        </ul>
+      <div className={styles.mainPanel}>
+        <RedditPanel reddit={reddit} />
       </div>
-    </div>
   )
 };
 
 MainPanel.propTypes = {
-  subreddit: PropTypes.array.isRequired,
+  reddit: PropTypes.array.isRequired,
+  productHunt: PropTypes.array.isRequired
 };
 
 export default MainPanel;
