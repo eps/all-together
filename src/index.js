92570d0b59c50b8e6c0adce0bdf40c0a28d5df6e
Herman Chan <hermchan89@gmail.com>
2018-03-02 15:30:31 -0800

    import react-routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './containers/App/App';
+import RedditPanel from './components/RedditPanel/RedditPanel';
+import ProductHuntPanel from './components/ProductHuntPanel/ProductHuntPanel';
 import './index.scss';
 
-ReactDOM.render(<App />,document.getElementById('root'));
+const router = (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/reddit" component={RedditPanel} />
+      <Route path="/product-hunt" component={ProductHuntPanel} />
+    </Switch>
+  </Router>
+)
+
+ReactDOM.render(router,document.getElementById('root'));
