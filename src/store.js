7c79a066d5166508d24e0334c4cb62ad213545a7
Herman Chan <hermchan89@gmail.com>
2018-03-15 19:10:58 -0700

    installed redux dev tool

--- src/store.js
+++ src/store.js
@@ -1,6 +1,9 @@
-import { createStore } from "redux";
+import { createStore } from 'redux';
 import rootReducer from "./reducers/index";
+import { devToolsEnhancer } from "redux-devtools-extension";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, /* preloadedState, */ devToolsEnhancer(
+  // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+));
 
 export default store;
