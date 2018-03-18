5879a60b60517c6dd1001272651e10287e0773b7
Herman Chan <hermchan89@gmail.com>
2018-03-17 19:45:11 -0700

    created new initial main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,27 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-// import styles from './Main.scss';
-import RedditSection from '../RedditSection/RedditSection';
-import ProductHuntSection from '../ProductHuntSection/ProductHuntSection';
-
+// import PropTypes from 'prop-types';
+import styles from './Main.scss';
+// import RedditSection from '../RedditSection/RedditSection';
+// import SideBarSection from '../SideBarSection/SideBarSection';
 
 class Main extends React.Component {
   render() {
-    const links = this.props.selectedBrowse;
-    console.log(links)
-    switch(links) {
-    case 'reddit':
-      return <RedditSection />
-    case 'product hunt':
-      return <ProductHuntSection />
-    default:
-      return <div>home</div>
-    }
+    return (
+      <div className={styles.mainPanel}>
+        main section
+      </div>
+    )
   }
-}
-
-Main.propTypes = {
-  selectedBrowse: PropTypes.string.isRequired
 }
 
 export default Main;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import SideBarSection from '../../components/SideBarSection/SideBarSection';
-// import Main from '../../components/Main/Main';
+import Main from '../../components/Main/Main';
 import styles from './App.scss';
 
 const App = () => (
   <div className={styles.container}>
     <SideBarSection />
+    <Main />
   </div>
 );
 
